243bed03a6c5f1ebefeac79924d54a1db315d228
Chris <creynolds@rbr-tech.com>
2017-08-30 13:18:28 -0400

    SpringMvcWeb lesson10 completed

--- src/main/java/app1/controllers/WelcomeController.java
+++ src/main/java/app1/controllers/WelcomeController.java
@@ -1,11 +1,18 @@
 package app1.controllers;
 
+import com.google.gson.Gson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import app1.model.UserInfo;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +20,7 @@ import app1.utilities.SpringAppContextUtils;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
+import java.util.ArrayList;
 import java.util.Properties;
 
 @Controller
@@ -111,5 +119,188 @@ public class WelcomeController
     }
 
 
+    /***************************************************************************
+     * getUsers()   *REST EndPoint*
+     *  1) Run SQL to get a List of UserInfo objects
+     *  2) Use the GSON library to convert the list into a JSON string
+     *  3) Return the JSON string
+     *
+     * ASSUMPTION:  You have the GSON library dependecy added:
+     *  	     <dependency>
+     *  	          <groupId>com.google.code.gson</groupId>
+     *  	          <artifactId>gson</artifactId>
+     *  	          <version>2.6.2</version>
+     *  	     </dependency>
+     ****************************************************************************/
+    @RequestMapping(value="/rest/users", method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity<?> getUsers()
+    {
+        logger.debug("getUsers() started");
 
+        try
+        {
+            // Run a SQL call to get the list of users
+            ArrayList<UserInfo> users = getUserListOrderedBy("name");
+
+            // Convert the list of UserInfo into a JSON string
+            Gson gson = new Gson();
+            String sJson = gson.toJson(users);
+
+            // Return respnose code of 200 and the JSON string
+            return new ResponseEntity<String>(sJson, HttpStatus.OK);
+        }
+        catch (Exception e)
+        {
+            // Tell the AJAX call that this call failed
+            logger.error("Error occurred making rest call to /rest/users", e);
+
+            // Get a formatted error message from the exception object
+            String sMessage = getFormattedMessageFromException(e);
+
+            // Tell the AJAX caller that this will be plain text being returned (and not JSON)
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.TEXT_PLAIN);
+
+            // Return the error back to the caller
+            return new ResponseEntity<String>(sMessage, headers, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    /***************************************************************************
+     * getUsers2()   *REST EndPoint*
+     *  1) Run SQL to get a List of UserInfo objects
+     *  2) Have Spring Convert the list into a JSON string
+     *
+     * ASSUMPTION:  You have a dependency that will convert an object to JSON
+     *              such as
+     *
+     *  	    	 <dependency>
+     *  	            <groupId>com.google.code.gson</groupId>
+     *  	            <artifactId>gson</artifactId>
+     *  	            <version>2.6.2</version>
+     *  	    	 </dependency>
+     *
+     *  	        *OR*
+     *
+     *  	     	<dependency>
+     *  	           <groupId>com.fasterxml.jackson.core</groupId>
+     *  	           <artifactId>jackson-databind</artifactId>
+     *  	           <version>2.8.1</version>
+     *  	      	</dependency>
+     *  	      	<dependency>
+     *  	           <groupId>javax.xml.bind</groupId>
+     *  	           <artifactId>jaxb-api</artifactId>
+     *  	           <version>2.2.12</version>
+     *  	      	</dependency>
+     ****************************************************************************/
+    @RequestMapping(value="/rest/users2", method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity<?> getUsers2()
+    {
+        logger.debug("getUsers() started");
+
+        try
+        {
+            // Run a SQL call to get the list of users
+            ArrayList<UserInfo> users = getUserListOrderedBy("name");
+
+            // Return the array of Users and a status code of 200 (OK)
+            // NOTE:  Spring will convert this to JSON for us automatically
+            return new ResponseEntity<ArrayList<UserInfo>>(users, HttpStatus.OK);
+        }
+        catch (Exception e)
+        {
+            // Tell the AJAX call that this call failed
+            logger.error("Error occurred making rest call to /rest/users2", e);
+
+            // Get a formatted error message from the exception object
+            String sMessage = getFormattedMessageFromException(e);
+
+            // Tell the AJAX caller that this will be plain text being returned (and not JSON)
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.TEXT_PLAIN);
+
+            // Return the error back to the caller
+            return new ResponseEntity<String>(sMessage, headers, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+
+    /***************************************************************************
+     * getUserListOrderedBy()
+     *  1) Run a sql call to get all userinfo from the database
+     *  2) Loop through the results, creating UserInfo objects
+     *
+     *  Returns a list of UserInfo objects
+     ****************************************************************************/
+    private ArrayList<UserInfo> getUserListOrderedBy(String aOrderBy) throws Exception
+    {
+        ArrayList<UserInfo> users = new ArrayList<UserInfo>();
+
+        // Construct the SQL call
+        final String sSql = "Select name from users order by " + aOrderBy;
+
+        JdbcTemplate jt = new JdbcTemplate(this.postgresDataSource);
+
+        // Get a connection from the JDBC pool, run the query, return the connection to the pool
+        SqlRowSet rs = jt.queryForRowSet(sSql);
+
+        while (rs.next())
+        {
+            // Get the name from the read-only recordset
+            String sUserName = rs.getString(1);
+
+            // Construct a new userInfo object and popuplate it with data from the database
+            UserInfo userInfo = new UserInfo();
+            userInfo.setUserName(sUserName);
+            userInfo.setIsAdministrator(false);
+
+            users.add(userInfo);
+        }
+
+        return users;
+    }
+
+
+    /***************************************************************************
+     * getFormattedMessageFromException()
+     *
+     * Take the passed-in exception object and return a formatted string
+     ****************************************************************************/
+    private String getFormattedMessageFromException(Exception aException)
+    {
+        StringBuilder sbMessage = new StringBuilder();
+
+        String sStackTrace = getStackTraceAsString(aException);
+
+        sbMessage.append("Message: ")
+                .append(aException.getMessage())
+                .append("\n\n")
+                .append("Cause: ")
+                .append(aException.getCause())
+                .append("\n\n")
+                .append("StackTrace:\n")
+                .append(sStackTrace);
+
+        return sbMessage.toString();
+    }
+
+
+
+    /***************************************************************************
+     * getStackTraceAsString()
+     *
+     * Take the passed-in exception object and generate a stack trace as
+     * a string separated by \n
+     ****************************************************************************/
+    public String getStackTraceAsString(Exception aException)
+    {
+        StringBuilder sb = new StringBuilder();
+        for (StackTraceElement element : aException.getStackTrace()) {
+            sb.append(element.toString());
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
 }
